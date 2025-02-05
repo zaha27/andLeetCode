2bdd8cb9ccae47a36a9c75bdd876eb21949a8bdf
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-05 09:32:27 +0200

    2

--- easy/2.js
+++ easy/2.js
@@ -11,5 +11,25 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null) {
+        let x = (l1 !== null) ? l1.val : 0;
+        let y = (l2 !== null) ? l2.val : 0;
+        let sum = carry + x + y;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1 !== null) l1 = l1.next;
+        if (l2 !== null) l2 = l2.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+
+    return dummy.next;
 };
