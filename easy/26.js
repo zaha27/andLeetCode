4dd6a1c09eb330b41d87bd335da6d085a56b46fc
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-05 09:37:42 +0200

    updated 26

--- easy/26.js
+++ easy/26.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+    if (nums.length === 0) return 0;
+
+    let index = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    nums.length = index; // Redimensionăm array-ul pentru a elimina elementele duplicate
+    return index;
 };
