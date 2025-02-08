e5e814cad8a2c93fa949c67f398d0e84e5254169
Zaharia Andrei <74096925+zaha27@users.noreply.github.com>
2025-02-08 20:46:10 +0200

    Update 4.js

--- hard/4.js
+++ hard/4.js
@@ -1,74 +1,33 @@
 var findMedianSortedArrays = function(nums1, nums2) {
-    class MedianFinder {
-        constructor() {
-            this.maxHeap = [];
-            this.minHeap = [];
-        }
+    if (nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+    }
 
-        addNum(num) {
-            if (this.maxHeap.length === 0 || num <= -this.maxHeap[0]) {
-                this.maxHeap.push(-num);
-                this.heapifyUp(this.maxHeap);
+    let x = nums1.length, y = nums2.length;
+    let low = 0, high = x;
+
+    while (low <= high) {
+        let partitionX = Math.floor((low + high) / 2);
+        let partitionY = Math.floor((x + y + 1) / 2) - partitionX;
+
+        let maxX = (partitionX === 0) ? -Infinity : nums1[partitionX - 1];
+        let maxY = (partitionY === 0) ? -Infinity : nums2[partitionY - 1];
+
+        let minX = (partitionX === x) ? Infinity : nums1[partitionX];
+        let minY = (partitionY === y) ? Infinity : nums2[partitionY];
+
+        if (maxX <= minY && maxY <= minX) {
+            if ((x + y) % 2 === 1) {
+                return Math.max(maxX, maxY);
             } else {
-                this.minHeap.push(num);
-                this.heapifyUp(this.minHeap);
+                return (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
             }
-
-            if (this.maxHeap.length > this.minHeap.length + 1) {
-                this.minHeap.push(-this.maxHeap.shift());
-                this.heapifyUp(this.minHeap);
-                this.heapifyDown(this.maxHeap);
-            } else if (this.minHeap.length > this.maxHeap.length) {
-                this.maxHeap.push(-this.minHeap.shift());
-                this.heapifyUp(this.maxHeap);
-                this.heapifyDown(this.minHeap);
-            }
+        } else if (maxX > minY) {
+            high = partitionX - 1;
+        } else {
+            low = partitionX + 1;
         }
-
-        findMedian() {
-            if (this.maxHeap.length > this.minHeap.length) {
-                return -this.maxHeap[0];
-            }
-            return (-this.maxHeap[0] + this.minHeap[0]) / 2;
-        }
-
-        heapifyUp(heap) {
-            let idx = heap.length - 1;
-            while (idx > 0) {
-                let parentIdx = Math.floor((idx - 1) / 2);
-                if (heap[idx] > heap[parentIdx]) break;
-                [heap[idx], heap[parentIdx]] = [heap[parentIdx], heap[idx]];
-                idx = parentIdx;
-            }
-        }
-
-        heapifyDown(heap) {
-            let idx = 0;
-            const length = heap.length;
-            while (2 * idx + 1 < length) {
-                let leftChildIdx = 2 * idx + 1;
-                let rightChildIdx = 2 * idx + 2;
-                let smallerChildIdx = leftChildIdx;
-
-                if (rightChildIdx < length && heap[rightChildIdx] < heap[leftChildIdx]) {
-                    smallerChildIdx = rightChildIdx;
-                }
-
-                if (heap[idx] <= heap[smallerChildIdx]) break;
-                [heap[idx], heap[smallerChildIdx]] = [heap[smallerChildIdx], heap[idx]];
-                idx = smallerChildIdx;
-            }
-        }
-    };
-
-    const finder = new MedianFinder();
-
-    for (let num of nums1) {
-        finder.addNum(num);
     }
-    for (let num of nums2) {
-        finder.addNum(num);
-    }
-
-    return finder.findMedian();
+    
+    throw new Error("Input arrays are not sorted!");
 };
