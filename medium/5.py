c7aa39942d8870dc81676ee977aee723756a3fb4
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-09 17:20:40 +0200

    Update 5.py

--- medium/5.py
+++ medium/5.py
@@ -1,20 +1,22 @@
-def is_palindrome(s, left, right):
-    while left < right:
-        if s[left] != s[right]:
-            return False
-        left += 1
-        right -= 1
-    return True
+class Solution:
+    
+    def _is_palindrome(self, s: str, left: int, right: int) -> bool:
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
 
-def longestPalindrome(s):
-    n = len(s)
-    if n == 0:
-        return ""
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n == 0:
+            return ""
 
-    longest = ""
-    for i in range(n):
-        for j in range(i, n):
-            if is_palindrome(s, i, j):
-                if j - i + 1 > len(longest):
-                    longest = s[i:j+1]
-    return longest
+        longest = ""
+        for i in range(n):
+            for j in range(i, n):
+                if self._is_palindrome(s, i, j):
+                    if j - i + 1 > len(longest):
+                        longest = s[i:j+1]
+        return longest
