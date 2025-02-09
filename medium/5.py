ea4df3218e142f7d542e4d898009a70c7b1ef79a
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-09 17:38:48 +0200

    commit

--- medium/5.py
+++ medium/5.py
@@ -1,22 +1,23 @@
 class Solution:
     
-    def _is_palindrome(self, s: str, left: int, right: int) -> bool:
-        while left < right:
-            if s[left] != s[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
-
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        if n == 0:
+        if not s:
             return ""
 
+        def expandAroundCenter(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
         longest = ""
-        for i in range(n):
-            for j in range(i, n):
-                if self._is_palindrome(s, i, j):
-                    if j - i + 1 > len(longest):
-                        longest = s[i:j+1]
+        for i in range(len(s)):
+            odd_palindrome = expandAroundCenter(s, i, i)
+            if len(odd_palindrome) > len(longest):
+                longest = odd_palindrome
+
+            even_palindrome = expandAroundCenter(s, i, i + 1)
+            if len(even_palindrome) > len(longest):
+                longest = even_palindrome
+
         return longest
