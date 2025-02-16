778d4392534cea3cdf2ba92a3fbe2a97f903525e
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-16 16:15:21 +0200

    104

---
+++ easy/104.cpp
@@ -0,0 +1,16 @@
+/*
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return 1 + max(leftDepth, rightDepth);
+    }
+};
+
+*/
