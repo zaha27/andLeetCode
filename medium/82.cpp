b47a7fa3ad6859df2674c0ef09adfe2373f4a3cc
zaha27 <andrei.zaharia2005@gmail.com>
2025-03-23 00:26:10 +0200

    82

--- medium/82.cpp
+++ medium/82.cpp
@@ -1,14 +1,33 @@
+#include <unordered_map>
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return nullptr;
+
+        std::unordered_map<int, int> freq;
+
+        for (ListNode* p = head; p; p = p->next) {
+            freq[p->val]++;
+        }
+
         ListNode dummy(0);
         dummy.next = head;
 
         ListNode* prev = &dummy;
         ListNode* current = head;
 
-        while (current && current->next) {
-            
+        // A doua parcurgere: șterge nodurile cu valoare duplicată
+        while (current) {
+            if (freq[current->val] > 1) {
+                ListNode* temp = current;
+                current = current->next;
+                delete temp;
+                prev->next = current;
+            } else {
+                prev = current;
+                current = current->next;
+            }
         }
 
         return dummy.next;
